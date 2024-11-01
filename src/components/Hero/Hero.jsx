65621f8a534f44f25db5b227bb9b1b7df7bf1f38
Ashutosh <ashutoshorisoh@gmail.com>
2024-11-01 22:23:26 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Ashutosh</h1>
+        <p className={styles.description}>
+        I'm a front-end developer who loves coding and creating user-friendly web apps. I'm constantly learning new things and enjoy working on both the front-end and back-end to bring my projects to life and make everything run smoothly.
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/newt.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
